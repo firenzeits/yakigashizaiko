1fb5217729d2b482ef7298a70e757c1aed4ac222
ryuichi kojima <ryuichi_kojima@firenze.co.jp>
2019-03-05 18:49:15 +0900

    20190305

--- mysite/settings.py
+++ mysite/settings.py
@@ -151,10 +151,3 @@ STATICFILES_DIRS = (
 STATIC_ROOT = 'staticfiles'
 #STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
